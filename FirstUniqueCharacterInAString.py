9a52563714c4807785106ce0b910981ec42092b3
Raksana Allahverdiyeva <reksaneallahverdiyeva4@gmail.com>
2024-03-23 19:19:21 +0400

    FirstUniqueChar

---
+++ FirstUniqueCharacterInAString.py
@@ -0,0 +1,22 @@
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for i in range(len(s)):
+            if s[i] not in s[:i]+s[i+1:]:
+                return i
+        return -1
+    
+    
+# Example 1:
+
+# Input: s = "leetcode"
+# Output: 0
+# Example 2:
+
+# Input: s = "loveleetcode"
+# Output: 2
+# Example 3:
+
+# Input: s = "aabb"
+# Output: -1
